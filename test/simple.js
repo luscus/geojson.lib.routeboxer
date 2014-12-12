d93689a472ff4fd797e170c99f6e18bec0278103
luscus <luscus.redbeard@gmail.com>
2014-12-12 16:10:13 +0100

    route works

--- test/simple.js
+++ test/simple.js
@@ -6,9 +6,15 @@ var geojson = {
   "type": "LineString",
   "coordinates": [
     [100.0, 0.0],
+    [100.25, 0.5],
+    [100.73, 0.63],
+    [100.90, 0.95],
     [101.0, 1.0]
   ]};
-var range = 10;
+
+var route = require('./route.json');
+
+var range = 20;
 var boxes = boxer.box(geojson, range);
 
 
